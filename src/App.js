3057a59fd6a9073c31cf3c62e1520184bb27ea1f
KimDaeHyun <90908005+LifeIsRightward@users.noreply.github.com>
2024-09-02 01:24:47 +0900

    [Developing] Introduce init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header';
 import Slide from './components/slide';
+import Introduce from './components/introduce';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
       <Slide />
+      <Introduce />
     </div>
   );
 }
---
+++ src/components/introduce.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Introduce() {
+    return (
+        <div className="introduce-container">
+            <div className="introduce-img">
+
+            </div>
+            <div className="introduce-text">
+
+            </div>
+        </div>
+
+    );
+}
